98565e6a13bc4c935f861e0d060fc4eaf49f6b4e
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-04-25 13:22:46 -0700

    start rewriting gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,136 +1,51 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var reload  = browserSync.reload;
-var minifyCss = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
-var rename = require("gulp-rename");
-const imagemin = require('gulp-imagemin');
-var svgstore = require('gulp-svgstore');
-var svgmin = require('gulp-svgmin');
-var path = require('path');
-var concat = require("gulp-concat");
+const { watch, src, dest } = require("gulp")
+const uglify = require("gulp-uglify")
+const rename = require("gulp-rename")
+const concat = require("gulp-concat")
+const autoprefixer = require("gulp-autoprefixer")
+const sass = require("gulp-sass")(require("sass"))
+const cleanCSS = require("gulp-clean-css")
+const browsersync = require('browser-sync').create();
 
-
-
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'js', 'compressImg', 'svgstore'], function() {
-
-    browserSync({
-			proxy:
-				"http://localhost:10084/thenewsbeat/article-experiment/article-list.html",
-		})
-    // Watch SCSS file for change to pass on to sass compiler,
-    gulp.watch('assets/sass/*.{scss,sass}', ['sass']);
-    // Watch SCSS file for change to pass on to sass compiler,
-    gulp.watch('assets/js/*.js', ['js']);
-    // run img compression when images added to directory
-    gulp.watch('assets/img/*.*', ['compressImg']);
-    // run SVG when svg files added
-    gulp.watch('assets/svg/*.svg', ['svgstore']);
-    // Watch our CSS file and reload when it's done compiling
-    gulp.watch("dist/css/*.css").on('change', reload);
-    // Watch php file
-    gulp.watch("*/*.php").on('change', reload);
-    // watch javascript files
-    gulp.watch("dist/js/*.js").on('change', reload);
-});
-
-gulp.task('svgstore', function () {
-    return gulp
-        .src('assets/svg/*.svg')
-        .pipe(svgmin(function (file) {
-            var prefix = path.basename(file.relative, path.extname(file.relative));
-            return {
-                plugins: [{
-                    cleanupIDs: {
-                        prefix: prefix + '-',
-                        minify: true
-                    }
-                }]
-            };
-        }))
-        .pipe(svgstore({ inlineSvg: true }))
-        .pipe(gulp.dest('dist/svg/'));
-});
-
-/* gulp.task('sass', function () {
-  // gulp.src locates the source files for the process.
-  // This globbing function tells gulp to use all files
-  // ending with .scss or .sass within the scss folder.
-  return gulp.src('stylesheets/*.{scss,sass}')
-    // Initializes sourcemaps
-    .pipe(sourcemaps.init())
-    // Converts Sass into CSS with Gulp Sass
-    .pipe(sass({
-      errLogToConsole: true
-    }))
-    // adds prefixes to whatever needs to get done
-    .pipe(autoprefixer())
-    // Writes sourcemaps into the CSS file
-    .pipe(sourcemaps.write())
-    // Outputs CSS files in the css folder
-    .pipe(gulp.dest(''));
-})*/
-
-
-gulp.task('sass', function () {
-    processSASS('styles');
-});
-
-gulp.task('js', function() {
-    var jsFiles = 'assets/js/*.js',
-    jsDest = 'dist/js';
-
-    return gulp.src(jsFiles)
-        .pipe(concat('scripts.js'))
-        .pipe(gulp.dest(jsDest))
-        .pipe(rename('scripts.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(jsDest));
-});
-
-function compressJS(filename) {
-    rootPath = "assets/js/";
-    src = "assets/js/"+filename+".js";
-    dist = 'dist/js/';
-
-    return gulp.src(src)
-        .pipe(uglify())
-        .pipe(rename({
-          suffix: '.min'
-        }))
-        .pipe(gulp.dest(dist));
+function css(filename) {
+	const source = "assets/sass/" + filename + ".{scss,sass}"
+	return src(source)
+		.pipe(sass().on("error", sass.logError))
+		.pipe(autoprefixer())
+		.pipe(cleanCSS({ compatibility: "ie8" }))
+		.pipe(
+			rename({
+				suffix: ".min",
+			})
+		)
+		.pipe(dest("dist/css/"))
 }
 
-gulp.task('compressImg', function() {
-    return gulp.src('assets/img/*')
-            .pipe(imagemin())
-            .pipe(gulp.dest('dist/img'));
-});
-
-
-function processSASS(filename) {
-    return gulp.src('assets/sass/'+filename+'.{scss,sass}')
-      // Converts Sass into CSS with Gulp Sass
-      .pipe(sass({
-        errLogToConsole: true
-      }))
-      // adds prefixes to whatever needs to get done
-      .pipe(autoprefixer())
-
-      // minify the CSS
-      .pipe(minifyCss())
-
-      // rename to add .min
-      .pipe(rename({
-        suffix: '.min'
-      }))
-      // Outputs CSS files in the css folder
-      .pipe(gulp.dest('dist/css/'));
+function js() {
+  const source = "assets/js/*.js"
+	return src(source)
+		.pipe(concat("scripts.js"))
+		.pipe(rename("scripts.min.js"))
+		.pipe(uglify())
+		.pipe(dest("dist/js"))
 }
 
-// Creating a default task
-gulp.task('default', ['serve']);
+// Watch files
+function watchFiles() {
+    watch("assets/sass/*.{scss,sass}", css)
+    watch('assets/js/*.js', js);
+}
+
+// BrowserSync
+function browserSync() {
+    browsersync.init({
+        server: {
+            baseDir: './'
+        },
+        port: 3000
+    });
+}
+exports.watch = parallel(watchFiles, browserSync);
+exports.default = series(parallel(js, css))
+
+// series(defaultTask, javascript, css)
