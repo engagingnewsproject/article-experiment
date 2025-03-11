7b6d1cafdaa7d47fa844a3e45de51cf636bf81e1
Luke C Hartman <luke@lukecarlhartman.com>
2025-03-11 13:40:01 -0700

    video pitch

---
+++ articles/article-video-pitch/index.php
@@ -0,0 +1,6 @@
+<?php
+include ('../../config.php');
+include ('data.php');
+// TEMPLATE_PATH is referring to the key `templatePath` 
+// set in `config.json` (constant defined in `config.php`)
+include (TEMPLATE_PATH);
